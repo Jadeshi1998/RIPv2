282cb4eb9e9e2031c5da703cf35c7c30f3e97881
szh139 <szh139@uclive.ac.nz>
2025-02-24 22:28:48 +1300

    make config and fix router1

--- config.py
+++ config.py
@@ -5,12 +5,37 @@ import re
 
 def read_config(filename):
     # read some file of router_id and input_ports
-    # config={router_id=None, input_ports=[], outputs=[]}
-    lines=[]
+
+    router_txt=[]
+    input_ports=[]
+    output_ports=[]
     file=open(filename,'r')
     for line in file.readlines():
         line = re.split(', | |\n',line)
-        print(line)
-        lines.append(line)
-    print(lines)
+        router_txt.append(line)
+    print(router_txt)
+    
+    #get input_ports
+    for port in router_txt[1][1:-1]:
+        if not (1024 <= int(port) <= 64000):
+            raise ValueError(f"ERROR: Invalid input port {port}. Must be in range 1024-64000.")
+        # print(port)
+        input_ports.append(port)
+    print(input_ports)
+ 
+    #get output_ports
+    for port in router_txt[2][1:-1]:
+        port = re.split('-',port)
+        if not (1024 <= int(port[0]) <= 64000):
+            raise ValueError(f"ERROR: Invalid output port {port[0]}. Must be in range 1024-64000.")
+        output_ports.append(port)
+    print(output_ports)
+    
 
+
+def main():
+    config_filename = 'router1.txt'  
+    read_config(config_filename)
+
+if __name__ == "__main__":
+    main()
